02c27138dfa477008aca87a113d6e44e18b8f31f
Quini3 <squeenie.so@gmail.com>
2023-02-12 11:14:41 +0000

    corrected code

--- service/routes.py
+++ service/routes.py
@@ -110,7 +110,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-    return account.serialize(), status.HTTP_200_OK
+    return account.serialise(), status.HTTP_200_OK
 
 
 ######################################################################
