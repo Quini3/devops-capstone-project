12389558bfd19ac9a68fd91e83812b6f7b565edd
Quini3 <squeenie.so@gmail.com>
2023-02-12 11:28:20 +0000

    corrected code

--- service/routes.py
+++ service/routes.py
@@ -91,7 +91,7 @@ def get_account(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     
-    return account.serialise(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
@@ -107,10 +107,10 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    account.deserialize(request.get_json())
+    account.deserialise(request.get_json())
     account.update()
 
-    return account.serialise(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
